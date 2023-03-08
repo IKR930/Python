527e42e2d4caf7cf13b55252aa8c970d497167a5
Indra kiran reddy Vaddeboyana <indrakiranreddyvaddeboyana@Indras-MacBook-Air.local>
2023-03-08 11:25:13 -0600

    day1 answers

---
+++ day1.py
@@ -0,0 +1,57 @@
+#Exercise Level 2
+import math
+print(3+4) 
+print(3-4)
+print(3*4)
+print(3%4) #modulus
+print(3/4) #division
+print(3**4) #exponential
+print(3//4) # floor division
+
+#Level 3
+name = 'indra'
+family_name = 'vadde'
+country = 'india'
+print('I am enjoying 30 days of python')
+
+#Level4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('your name'))
+print(type('your family name'))
+print(type('your country'))
+
+#level 5
+
+n = 10
+f = 1.13
+c = 4 +4j
+name = "indra"
+married = False
+person = {
+    'sports': 'handball',
+    'love': 'soccer'
+}
+
+print(type(person))
+
+liste = [1,2,3,4,5,6,'indra']
+print(type(liste))
+sete = {0,2,4,6,8}
+print(type(sete))
+tup = (0,2,4,6,8,9,6)
+print(type(tup))
+
+a = [2]
+b = [3]
+distance = math.dist(a,b)
+print(distance)
+
+c = [10]
+d = [8]
+print(math.dist(c,d))
+
