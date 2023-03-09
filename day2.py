41de3d170372278636f74d87e5054649c8d5a4d3
Indra kiran reddy Vaddeboyana <indrakiranreddyvaddeboyana@Indras-MacBook-Air.local>
2023-03-09 11:17:26 -0600

    day2

--- day2.py
+++ day2.py
@@ -1,25 +1,54 @@
-import math
+print('Day2: 30 days of Python programming')
 
-
-first_name = "indra"
-print(type(first_name))
-last_name = "kiran"
-full_name = "indrakiran"
-country = "india"
-city = "hutto"
+first_name = 'Vaddeboyana'
+last_name = 'Indra Kiran'
+full_name = first_name + last_name
+country = 'India'
+city = 'Austin'
 age = 29
-is_married = False
+year = 2023
+is_married = True
+is_true = False
 is_light_on = True
-skills, company = "devops", "orrba"
+movie, song = 'creed', 'till i loose'
+
+
+#Level 2
+
+print(type(first_name,))
+
+print(len(full_name))
+print(first_name == last_name)
 
 num_one = 5
 num_two = 4
 total = num_one + num_two
 print(total)
-print("length of the first name is", len(first_name))
+diff = num_one - num_two
+print(diff)
+product = num_two * num_one
+print(product)
+remainder = num_two % num_one
+print(remainder)
+variable_exp = num_one ** num_two
+print(variable_exp)
+floor_division = num_one//num_two
+print(floor_division)
 
 
-a = 7
-b = 8
-c = a**b
-print(c)
+radius = float(input('enter the radius'))
+area = 3.14 * radius * radius
+circumference_of_circle = 3.14 * 2 * radius
+print(area)
+print(circumference_of_circle)
+
+
+
+
+first = input('first_name')
+last = input('last_name')
+country = input('country')
+age = input('age')
+help('keywords')
+
+
