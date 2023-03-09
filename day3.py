6d6ee8d63ba48c70ab4ba162d783223c7fae827b
Indra kiran reddy Vaddeboyana <indrakiranreddyvaddeboyana@Indras-MacBook-Air.local>
2023-03-09 12:21:13 -0600

    day3

--- day3.py
+++ day3.py
@@ -1,30 +1,49 @@
-from math import pi
-
 age = 29
-#height = 5.7
-something = 1 + 1j
-b = int(input("base"))
-h= int(input("height"))
-area_of_traingle = 0.5*b*h
-
-print("area =", area_of_traingle)
+height = 5.6
+base = int(input('enter the base'))
+height = int(input('enter the height'))
+area = 0.5 * base * height
+print('area of the triangle is' , area)
 
 
+a = int(input('enter side a:'))
+b = int(input('enter side b:'))
+c = int(input('enter side c:'))
+perimeter = a + b + c
+print('perimeter of traingele is',perimeter)
 
-a = 6
-b = 7
-c = a+b
-perimeter_of_traingle = c
-print("perimeter of triangle =" , perimeter_of_traingle)
+l = int(input('enter the lenght of reactangle'))
+w = int(input('enter the width of rectangle'))
+area = l * w
+print('area of reactangle is ', area)
+perimeter = 2 * (l + w)
+print('perimeter of rectangle is', perimeter)
 
-length = 55
-breadth = 44
-area_of_rectangle = length*breadth
-print("are of rectangle =", area_of_rectangle)
-perimeter_of_rectangle = 2*(length + breadth)
-print("perimeter of rectangle =" ,perimeter_of_rectangle)
+for x in range(0,10):
+    print(x ** 2 + 6 * x + 9) #doesn't really make sense to me
 
+print(len ('python') != len ('dragon'))
 
-r = float(input ("Input the radius of the circle : "))
-print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+print('on' in 'python' and 'on' in 'dragon')
 
+print('jargon' in 'I hope this course is not full of jargon')
+
+print('on' not in 'dragon', 'on' not in 'python')
+
+p = len('python')
+p_c = float(p)
+print(p_c)
+
+number = int(input('the number is '))
+print('even' if number % 2 == 0 else 'odd')
+
+s = 7//3
+print(s == int(2.7))
+
+hours = int(input('enter hours'))
+rph = int(input('enter rate per hour'))
+weekly_earnings = hours * rph
+print('weekly earnings are:', weekly_earnings)
+
+for i in range(1, 6):
+    print(i, i ** 0, i ** 1, i ** 2, i ** 3)
